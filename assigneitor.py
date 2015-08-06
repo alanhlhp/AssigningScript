44f755a1a9992feac653c75bf22b543180849f49
Alan H. Zacarias <alanh.lhp@gmail.com>
2015-08-06 08:02:56 -0500

    Update assigneitor.py

--- assigneitor.py
+++ assigneitor.py
@@ -30,3 +30,7 @@ select_dropdown(driver, "allComm", "322555") # Selects the community
 select_dropdown(driver, "selIdProcessAjax", "1820234") # Selects the process
 select_dropdown(driver, "selIdStepAjax", "1820246") #Selects the step
 driver.find_element_by_xpath("//a[contains(@href, 'fnSubmit')]").click() #clickbutton
+
+table = driver.find_element_by_xpath("//table[@bgColor = '#dddddd']")
+rowCount = table.find_elements_by_xpath("//tbody/tr[@bgColor = '#ffffff']") #length of table
+print len(rowCount)
